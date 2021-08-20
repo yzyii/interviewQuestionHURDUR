ae9fba16b581544b05028c61e2a527b3160b7e87
yzyii <mark.yz.yii@gmail.com>
2021-08-20 01:34:51 -0700

    Update howdoishotweb.java

--- howdoishotweb.java
+++ howdoishotweb.java
@@ -28,8 +28,10 @@ class Main {
         providers.add(e);
         
         // Let's make this an entire class so it's easier for you noobs...
+        // If it weren't clear already, you need to implement this class and the getNextJob() method within it below.
         ImplementMe implementMe = new ImplementMe(providers);
         
+        // This final loop should print out the first 1000 jobs from all job providers in the correct order.
         System.out.print("Here we go! (the following should be in order):\n");
         for (int i = 0; i < 1000; i++) {
             System.out.print(implementMe.getNextJob().datetime + "\n");
@@ -45,6 +47,8 @@ class Main {
     }
 }
 
+// TODO: IMPLEMENT THIS CLASS / THE getNextJob() METHOD
+// DO NOT TOUCH ANY CODE EXCEPT WHAT IS IN THIS ImplementMe CLASS.
 class ImplementMe {
     List<JobProvider> providers;
     
@@ -53,12 +57,14 @@ class ImplementMe {
     }
     
     public Job getNextJob() {
-        // TODO: IMPLEMENT THIS.
-        // THE OUTPUT SHOULD BE:
+        // TODO: IMPLEMENT THIS CLASS / THE getNextJob() METHOD
+        // THE OUTPUT WHEN PRINTED CONTINUALLY IN A LOOP SUCH AS IN MAIN SHOULD BE:
         // 4,5,6,8,8,11,11,12,16,17 etc...
         return new Job(-1);
     }
 }
+
+// DO NOT TOUCH ANYTHING BELOW HERE
 
 class Job {
     long datetime;
